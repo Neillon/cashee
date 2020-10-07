23765fe08ff24b280ebd5c9930aa116f27313912
Neillon Cesar <neilloncesar13@gmail.com>
2020-10-07 08:22:43 -0300

    Adding enable functionality into NextButton

--- features/authentication/src/main/java/com/neillon/cashee/authentication/ui/custom/NextButton.kt
+++ features/authentication/src/main/java/com/neillon/cashee/authentication/ui/custom/NextButton.kt
@@ -8,30 +8,33 @@ import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.neillon.authentication.R
 
-
 class NextButton(context: Context, var attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
     private lateinit var title: String
+    private var enableButton: Boolean
 
     private val textViewTitle: TextView by lazy { findViewById<TextView>(R.id.buttonTitleNextButton) }
 
     init {
         val inflater = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
-        inflater.inflate(
-            R.layout.custom_next_button,
-            this@NextButton,
-            true
-        )
-        background = getDrawable(context, R.drawable.background_next_button)
+        inflater.inflate(R.layout.custom_next_button, this@NextButton, true)
 
         context.theme.obtainStyledAttributes(attrs, R.styleable.NextButton, 0, 0)
             .apply {
                 try {
+                    enableButton = getBoolean(R.styleable.NextButton_enable, false)
                     title = getString(R.styleable.NextButton_title).toString()
                 } finally {
                     recycle()
                 }
             }
+
         textViewTitle.text = title
+        background = if (enableButton) {
+            getDrawable(context, R.drawable.background_next_button)
+        } else {
+            getDrawable(context, R.drawable.background_next_button_disabled)
+        }
+
     }
 }
